2d711889a96959c96983b72839335a9a6c7a4480
Maksym Kosenko <maksym.kosenko2014@gmail.com>
2024-09-12 18:12:18 +0300

    add

---
+++ src/basic/3.ts
@@ -0,0 +1,13 @@
+let union: string | number;
+
+union = "string"; // ok
+union = 15; // ok
+// union = true; //Error
+
+// ========================================================
+
+let literal: 'enable' | 'disable';
+
+literal = 'enable'; // ok
+literal = 'disable'; // ok
+// literal = 'literal'; // Error
